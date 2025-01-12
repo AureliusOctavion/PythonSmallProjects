53ea6aef154ed6c1ac1bd227c3427797f23d0442
Caesar <126032598+AureliusOctavion@users.noreply.github.com>
2025-01-12 15:27:24 -0600

    Add files via upload

---
+++ 05_02_yeehaw.py
@@ -0,0 +1,7 @@
+# In this script, write code that displays the follow message:
+#
+# 	I'm a programmer now.
+# 	Yeehaw!
+# 	Coding here I come!
+
+print("I'm a programer now. \nYeehaw! \nCoding here I come!")
