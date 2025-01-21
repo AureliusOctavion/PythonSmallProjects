8d84ec33180e8af118c35cb0f22aee8a58a512d9
Caesar <126032598+AureliusOctavion@users.noreply.github.com>
2025-01-20 18:09:31 -0600

    Add files via upload

---
+++ 06_02_fix_missing_pass.py
@@ -0,0 +1,7 @@
+# The code below runs into an error.
+# Fix it, without making Python do anything.
+
+flag = False
+while flag:
+    print("all done.")
+
---
+++ 06_05_program_pseudocode.py
@@ -0,0 +1,15 @@
+# Think about a task that you want to accomplish using programming
+# Maybe you want to automate something you do every day on your computer?
+# Break down some of the steps you can think of and write them in
+# this file as pseudocode.
+# You can come back and update this file when you've learned more.
+# You can also add real Python code after learning the necessary concepts.
+# For now, just practice breaking larger tasks into smaller steps
+# and writing out your thoughts in pseudocode.
+
+# Task that I want to accomplish using programming: Keybind function that is pressed at start that opens up all 
+# recently closed tabs that doesnt require me to click anything.
+
+# Step 1: Press the esc button 
+# Step 2: Script upon the key being pressed runs a automatic script that runs code to open up all last used chrome tabs
+# Step 3: Slowly open up all last used tabs on chrome rather then indivudally going into history and opening them
